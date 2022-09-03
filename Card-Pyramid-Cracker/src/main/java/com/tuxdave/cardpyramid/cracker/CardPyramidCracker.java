6a7adaa4097621383df5e7926cb9f5a1cbc6ab0c
tuxdave <godinodavide@gmail.com>
2022-09-03 11:51:45 +0200

    modificato MAIN

--- Card-Pyramid-Cracker/src/main/java/com/tuxdave/cardpyramid/cracker/CardPyramidCracker.java
+++ Card-Pyramid-Cracker/src/main/java/com/tuxdave/cardpyramid/cracker/CardPyramidCracker.java
@@ -5,7 +5,7 @@ import com.tuxdave.cardpyramid.cracker.tree.UiKt;
 
 public class CardPyramidCracker {
     public static void main(String[] args) {
-        FakeTree tree = CrackerKt.crack(8);
+        FakeTree tree = CrackerKt.crack(Integer.parseInt(args[0]));
         System.out.println("Albero calcolato");
         System.out.println(UiKt.display(tree));
     }
