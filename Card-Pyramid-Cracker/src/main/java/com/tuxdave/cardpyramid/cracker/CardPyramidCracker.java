92eaef0afabdd82da057b32a09e242f959d6779e
tuxdave <godinodavide@gmail.com>
2022-09-11 15:33:54 +0200

    Aggiornato printer dell'albero per "provarci" anche con gli alberi troppo grandi

--- Card-Pyramid-Cracker/src/main/java/com/tuxdave/cardpyramid/cracker/CardPyramidCracker.java
+++ Card-Pyramid-Cracker/src/main/java/com/tuxdave/cardpyramid/cracker/CardPyramidCracker.java
@@ -7,6 +7,6 @@ public class CardPyramidCracker {
     public static void main(String[] args) {
         FakeTree tree = CrackerKt.crack(Integer.parseInt(args[0]));
         System.out.println("Albero calcolato");
-        System.out.println(UiKt.display(tree));
+        UiKt.display(tree);
     }
 }
--- Card-Pyramid-Cracker/src/main/java/com/tuxdave/cardpyramid/cracker/tree/Ui.kt
+++ Card-Pyramid-Cracker/src/main/java/com/tuxdave/cardpyramid/cracker/tree/Ui.kt
@@ -7,27 +7,29 @@ fun Node.display(tabs: Int = 0): String{
     return ("\t".repeat(tabs)) + Arrays.deepToString(this.state.toTypedArray())
 }
 
-fun FakeTree.display(): String{
+fun FakeTree.display(): Unit{
     var c = 0
     var ret = "ROOT: ${this.root.display()}\n\n===================\nAFTER ${c++} MOVE\n"
-    ret += explodeNode(root)
-    return ret
+    print(ret)
+    explodeNode(root)
+//    return ret
 }
 
 var tabs = 0;
-fun explodeNode(nodo: Node, base: String = ""): String{
-    var ret = base + nodo.display(tabs++) +  "\n"
+fun explodeNode(nodo: Node, base: String = ""): Unit{
+    var ret = nodo.display(tabs++) +  "\n"
     if(nodo.siblings.size == 0){
         tabs--
     }
     var c = 0
+    println(ret)
     for(son in nodo.siblings){
-        ret += explodeNode(son)
+        explodeNode(son)
         //questo controllo per via della memoization fa attivare il controllo anche se non è effettivamente l'ultimo
         if(son == nodo.siblings.last() && c == nodo.siblings.lastIndexOf(son)){
             tabs--
         }
         c++;
     }
-    return ret
+//    return ret
 }
